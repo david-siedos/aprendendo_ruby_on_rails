99415d3483357400080e4be41376e1e7bb0db773
david-siedos <david.andrade@siedos.com.br>
2021-09-17 14:52:54 -0300

    I18n Mudar idioma na pagina / Ate aula 85

--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-     def data_br(data_us)
-        data_us.strftime("%d/%m/%Y")
-     end
+   def locale
+      I18n.locale == :en ? "Estados Unidos" : "Portugues do Brasil"
+   end
+
+   def data_br(data_us)
+      data_us.strftime("%d/%m/%Y")
+   end
 end
